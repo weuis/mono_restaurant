14ee4fbfb5dc3eac1f5efc8e7c0caeccfb1589cb
Алексей Розеган <rozeganalexey@gmail.com>
2025-04-08 17:38:53 +0200

    Add model to admin panel & customize it

--- mono_app/admin.py
+++ mono_app/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import DishType, Ingredient, Cook, Dish
 
-# Register your models here.
+@admin.register(DishType)
+class DishTypeAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ("name", "is_allergen", "unit", "quantity")
+    list_filter = ("is_allergen", "unit")
+    search_fields = ("name",)
+
+@admin.register(Cook)
+class CookAdmin(admin.ModelAdmin):
+    list_display = ("username", "first_name", "last_name", "specialization", "years_of_experience")
+    list_filter = ("specialization",)
+    search_fields = ("username", "first_name", "last_name", "specialization")
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_display = ("name", "dish_type", "price", "is_vegetarian")
+    list_filter = ("is_vegetarian", "dish_type")
+    search_fields = ("name", "description")
+    filter_horizontal = ("cooks", "ingredients")
+
