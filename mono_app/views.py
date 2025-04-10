6d212082298b9640f3cb98c94b68efeb2240fa46
Алексей Розеган <rozeganalexey@gmail.com>
2025-04-10 19:35:20 +0200

    Update view.py, index.html & base.html

--- mono_app/views.py
+++ mono_app/views.py
@@ -1,23 +1,22 @@
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-from .models import Dish, Cook, DishType
+from mono_app.models import Dish, Cook, DishType
 
 
-def index(request: HttpRequest):
-    num_dishes = Dish.objects.count()
-    num_cooks = Cook.objects.count()
-    num_dish_types = DishType.objects.count()
+def index(request: HttpRequest) -> HttpResponse:
+    dishes = Dish.objects.all()
+    cooks = Cook.objects.all()
+    dish_types = DishType.objects.all()
     latest_dishes = Dish.objects.select_related("dish_type").prefetch_related("cooks")[:6]
     top_cooks = Cook.objects.order_by("-years_of_experience")[:4]
-
     return render(
         request,
         "mono_app/index.html",
         context={
-            "num_dishes": num_dishes,
-            "num_cooks": num_cooks,
-            "num_dish_types": num_dish_types,
+            "dishes": dishes,
+            "cooks": cooks,
+            "dish_types": dish_types,
             "latest_dishes": latest_dishes,
             "top_cooks": top_cooks,
         }
