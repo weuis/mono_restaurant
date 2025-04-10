deadee4d24b6f433da84ec3581e4ab8a8d6d5138
Алексей Розеган <rozeganalexey@gmail.com>
2025-04-10 16:03:53 +0200

    Create view, template, url for index page

--- mono_app/urls.py
+++ mono_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-
+from mono_app.views import index
 
 urlpatterns = [
-    #path("", index, name="index"),
+    path("", index, name="index"),
 ]
+
+app_name = "mono_app"
--- mono_app/views.py
+++ mono_app/views.py
@@ -1,3 +1,24 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Dish, Cook, DishType
+
+
+def index(request: HttpRequest):
+    num_dishes = Dish.objects.count()
+    num_cooks = Cook.objects.count()
+    num_dish_types = DishType.objects.count()
+    latest_dishes = Dish.objects.select_related("dish_type").prefetch_related("cooks")[:6]
+    top_cooks = Cook.objects.order_by("-years_of_experience")[:4]
+
+    return render(
+        request,
+        "mono_app/index.html",
+        context={
+            "num_dishes": num_dishes,
+            "num_cooks": num_cooks,
+            "num_dish_types": num_dish_types,
+            "latest_dishes": latest_dishes,
+            "top_cooks": top_cooks,
+        }
+    )
