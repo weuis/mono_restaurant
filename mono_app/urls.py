ff1aef961d63d4b98ce8149dd3869eebdbd37661
Алексей Розеган <rozeganalexey@gmail.com>
2025-04-12 14:42:27 +0200

    Create url, view, template & navbar navigation for dishes list

--- mono_app/urls.py
+++ mono_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from mono_app.views import index
+from mono_app.views import index,DishListView
 
 urlpatterns = [
     path("", index, name="index"),
+    path('dishes/', DishListView.as_view(), name='dish-list'),
 ]
 
 app_name = "mono_app"
--- mono_app/views.py
+++ mono_app/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
 from django.contrib import messages
+from django.views import generic
 
 from mono_app.models import Dish, Cook, DishType
 
@@ -24,6 +25,33 @@ def index(request: HttpRequest) -> HttpResponse:
             "top_cooks": top_cooks,
         }
     )
+
+
+class DishListView(generic.ListView):
+    model = Dish
+    template_name = 'mono_app/dishes_list.html'
+    context_object_name = 'dishes'
+    paginate_by = 4
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        dish_type = self.request.GET.get('type')
+        search_query = self.request.GET.get('q')
+
+        if dish_type:
+            queryset = queryset.filter(dish_type__name__iexact=dish_type)
+
+        if search_query:
+            queryset = queryset.filter(name__icontains=search_query)
+
+        return queryset.order_by('-id')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['dish_types'] = DishType.objects.all()
+        context['selected_type'] = self.request.GET.get('type', '')
+        context['search_query'] = self.request.GET.get('q', '')
+        return context
 
 
 class CustomLoginView(LoginView):
