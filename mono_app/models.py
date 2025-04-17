f9110868e6fbe732c7a9d5911de6a60b41c851b9
Алексей Розеган <rozeganalexey@gmail.com>
2025-04-17 15:34:43 +0200

    Upload default pictures for profiles

--- mono_app/models.py
+++ mono_app/models.py
@@ -30,7 +30,7 @@ class Cook(AbstractUser):
     years_of_experience = models.PositiveIntegerField(default=0)
     bio = models.TextField(blank=True, null=True)
     specialization = models.CharField(max_length=100, blank=True, null=True)
-    profile_picture = models.ImageField(upload_to='cook_pics/', null=True, blank=True)
+    profile_picture = models.ImageField(upload_to='images/', null=True, blank=True)
 
     class Meta:
         verbose_name = "Cook"
--- mono_restaurant/settings.py
+++ mono_restaurant/settings.py
@@ -9,9 +9,10 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 from django.contrib.messages import constants as messages
+from django.template.context_processors import media
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,6 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
