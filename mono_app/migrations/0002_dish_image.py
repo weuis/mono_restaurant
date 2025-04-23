dc3cdf03fef1219167b6856066e43d15eacfb499
Алексей Розеган <rozeganalexey@gmail.com>
2025-04-23 11:24:00 +0200

    Remove __pycache__

--- mono_app/migrations/0002_dish_image.py
+++ mono_app/migrations/0002_dish_image.py
@@ -13,6 +13,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='dish',
             name='image',
-            field=models.ImageField(blank=True, null=True, upload_to='images/'),
+            field=models.ImageField(blank=True, null=True, upload_to='profile-image/'),
         ),
     ]
