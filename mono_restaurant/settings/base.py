b6e59ac182ece09f81aa88837abe57d87755717d
Алексей Розеган <rozeganalexey@gmail.com>
2025-05-11 16:57:22 +0200

    Update static files

--- mono_restaurant/settings/base.py
+++ mono_restaurant/settings/base.py
@@ -120,7 +120,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = 'staticfiles/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
